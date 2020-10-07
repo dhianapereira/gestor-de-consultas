3e9b6b8ff17a7655d9330465b7a90aa3c50d3fbb
Dhiana <dhianaslv@outlook.com>
2020-10-07 17:52:20 -0300

    bloc added

---
+++ ui/bloc/PatientBloc.php
@@ -0,0 +1,23 @@
+<?php
+    namespace ui\bloc;
+    use services\PatientService;
+    
+    class PatientBloc{
+        private $patient_service;
+
+        public function __construct()
+        {
+            $this->patient_service =  new PatientService();
+        }
+        
+        public function registerPatient($cpf, $full_name, $genre, $date_of_birth, $mother_name, $naturalness){
+            try {
+              $result = $this->patient_service->registrationService($cpf, $full_name, $genre, $date_of_birth, $mother_name, $naturalness);
+                
+              return $result;
+            } catch (Exception $e) {
+                return "Não foi possível realizar o cadastro do paciente. Tente mais tarde.";
+            }
+        }
+    }
+?>
