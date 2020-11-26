9eace72a4833b4a06d50b9c3200c706818d46a4a
Dhiana <dhianaslv@outlook.com>
2020-11-26 15:01:38 -0300

    bug fixes in the patient registration message

--- app/pages/patient/register_patient.php
+++ app/pages/patient/register_patient.php
@@ -3,51 +3,81 @@
 		<title>Resultado | Unidade de Saúde</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="#">
+        <link rel="shortcut icon" href="#"> 
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="../../../public/styles/css/main.css"
+        /> 
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="../../../public/styles/css/register_patient.css"
+        />
+        <link
+            rel="stylesheet"
+            type="text/css"
+            href="../../../public/styles/css/sidebar.css"
+        />
+        <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap"
+            rel="stylesheet"
+        />
 	</head>
     <body>
-        <?php
-            include_once('../../utils/autoload.php');
+        <div id="register-patient">
+            <aside class="animate-right sidebar">
+                <footer>
+                <button onclick="history.back()">
+                    <img src="../../../public/styles/img/arrow-back.svg" alt="Voltar" />
+                </button>
+                </footer>
+            </aside>
+            <main class="animate-appear with-sidebar">
+                <?php
+                    include_once('../../utils/autoload.php');
 
-            use app\controllers\PatientController;
+                    use app\controllers\PatientController;
 
-            $patient_controller = new PatientController();
+                    $patient_controller = new PatientController();
 
-            $cpf = addslashes($_POST["cpf"]);
-            $full_name = addslashes($_POST["full_name"]);
-            $genre = $_POST["genre"];
-            $date_of_birth = addslashes($_POST["date_of_birth"]);
-            $mother_name = addslashes($_POST["mother_name"]);
-            $naturalness = $_POST["naturalness"];
+                    $cpf = addslashes($_POST["cpf"]);
+                    $full_name = addslashes($_POST["full_name"]);
+                    $genre = $_POST["genre"];
+                    $date_of_birth = addslashes($_POST["date_of_birth"]);
+                    $mother_name = addslashes($_POST["mother_name"]);
+                    $naturalness = $_POST["naturalness"];
 
-            if(!isset($cpf) || !isset($full_name)
-             || !isset($genre) || !isset($date_of_birth) 
-             || !isset($mother_name) || !isset($naturalness)){
-        ?>
-                <div id='info'>
-                   <p>Você precisa preencher todos os campos para realizar esta operação!</p>
-                </div> 
-        <?php
-            }
-            else{
+                    if(!isset($cpf) || !isset($full_name)
+                    || !isset($genre) || !isset($date_of_birth) 
+                    || !isset($mother_name) || !isset($naturalness)){
+                ?>
+                        <div class='info'>
+                        <p>Você precisa preencher todos os campos para realizar esta operação!</p>
+                        </div> 
+                <?php
+                    }
+                    else{
 
-                $result = $patient_controller->registerPatient($cpf, $full_name, $genre, $date_of_birth, $mother_name, $naturalness);
+                        $result = $patient_controller->registerPatient($cpf, $full_name, $genre, $date_of_birth, $mother_name, $naturalness);
 
-                if(!is_object($result)){
-        ?>
-                    <div id='info'>
-                        <p><?php echo "$result" ?></p>
-                    </div>    
-        <?php 
-                } 
-                else{
-        ?>
-                    <div id='info'>
-                       <p>Seu cadastro foi realizado com sucesso!</p>
-                    </div>    
-        <?php 
-                }
-            }
-        ?>
+                        if(!is_object($result)){
+                ?>
+                            <div class='info'>
+                                <p><?php echo "$result" ?></p>
+                            </div>    
+                <?php 
+                        } 
+                        else{
+                ?>
+                            <div class='info'>
+                            <p>Seu cadastro foi realizado com sucesso!</p>
+                            </div>    
+                <?php 
+                        }
+                    }
+                ?>
+            </main>
+        </div>
     </body>
 </html>
