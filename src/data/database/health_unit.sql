77f43a4a6a5a9b4798462f98145cbf796c48e586
Dhiana <dhianaslv@outlook.com>
2021-01-05 17:41:42 -0300

    update database

--- src/data/database/health_unit.sql
+++ src/data/database/health_unit.sql
@@ -9,7 +9,7 @@ CREATE TABLE `patient` (
   `date_of_birth` VARCHAR(10) NOT NULL,
   `mother_name` VARCHAR(100) NOT NULL,
   `companion` VARCHAR(100) NOT NULL,
-  `patient_address` VARCHAR(100) NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
   `naturalness` VARCHAR(50) NOT NULL,
   CONSTRAINT `patient_pk` PRIMARY KEY (`cpf`)
 );
@@ -17,7 +17,7 @@ CREATE TABLE `patient` (
 CREATE TABLE `symptom` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `cpf_patient_fk` VARCHAR(14) NOT NULL,
-  `symptom_name` VARCHAR(100) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
    CONSTRAINT `symptom_pk` PRIMARY KEY (`id`),
    CONSTRAINT `symptom_fk` FOREIGN KEY (`cpf_patient_fk`)
    REFERENCES `patient` (`cpf`)
@@ -35,19 +35,34 @@ CREATE TABLE `medical_records` (
   REFERENCES `patient` (`cpf`)
 );
 
-CREATE TABLE `user` ( 
-  `id` INT(11) NOT NULL AUTO_INCREMENT, 
-  `name` VARCHAR(100) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  `user_password` VARCHAR(100) NOT NULL, 
-  `responsibility` VARCHAR(50) NOT NULL, 
-  CONSTRAINT `user_pk` PRIMARY KEY (`id`) 
-);
-
 CREATE TABLE `doctor` ( 
   `id` INT(11) NOT NULL AUTO_INCREMENT, 
   `name` VARCHAR(100) NOT NULL,
   `genre` VARCHAR(50) NOT NULL,
   `specialty` VARCHAR(100) NOT NULL, 
+  CONSTRAINT `user_pk` PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `medical_appointment` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `cpf_patient_fk` VARCHAR(14) NOT NULL,
+  `id_doctor_fk` INT(11) NOT NULL,
+  `time` VARCHAR(20) NOT NULL,
+  `date` VARCHAR(10) NOT NULL,
+  `arrival_time` VARCHAR(20),
+  `realized` BOOLEAN NOT NULL,
+  CONSTRAINT `medical_appointment_pk` PRIMARY KEY (`id`),
+  CONSTRAINT `medical_appointment_fk1` FOREIGN KEY (`cpf_patient_fk`)
+  REFERENCES `patient` (`cpf`), 
+  CONSTRAINT `medical_appointment_fk2` FOREIGN KEY (`id_doctor_fk`)
+  REFERENCES `doctor` (`id`) 
+);
+
+CREATE TABLE `user` ( 
+  `id` INT(11) NOT NULL AUTO_INCREMENT, 
+  `name` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL, 
+  `responsibility` VARCHAR(50) NOT NULL, 
   CONSTRAINT `user_pk` PRIMARY KEY (`id`) 
 );
