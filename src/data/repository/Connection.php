6e90f17e569a1760ed0450788195cfe44df26ccc
Dhiana <dhianaslv@outlook.com>
2021-01-06 16:54:30 -0300

    update connection

--- src/data/repository/Connection.php
+++ src/data/repository/Connection.php
@@ -1,15 +1,15 @@
 <?php
 namespace src\data\repository;
 
-define( 'HOST', 'localhost' );
-define( 'USER', 'root' );
-define( 'PASS', '' );
-define( 'DBNAME', 'health_unit' );
+define( 'HOST', "localhost" );
+define( 'USER', "root" );
+define( 'PASS', "" );
+define( 'DBNAME', "health_unit" );
 
 class Connection{
     private $connection;
     public function __construct() {
-        $this->connection = new \PDO( 'mysql:host=' . HOST . ';dbname=' . DBNAME, USER, PASS);
+        $this->connection = new \PDO( "mysql:host=" . HOST . ";dbname=" . DBNAME, USER, PASS);
     }
     
     public function connect(){
