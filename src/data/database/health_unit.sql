afacc83c9b38b710561f12a1adef1fe7cb2428c0
Dhiana <dhianaslv@outlook.com>
2021-01-20 15:17:01 -0300

    update database

--- src/data/database/health_unit.sql
+++ src/data/database/health_unit.sql
@@ -1,69 +1,72 @@
 CREATE DATABASE health_unit;
+
 USE health_unit;
 
-
-CREATE TABLE `patient` (
-  `cpf` VARCHAR(14) NOT NULL,
-  `full_name` VARCHAR(100) NOT NULL,
-  `genre` VARCHAR(50) NOT NULL,
-  `date_of_birth` VARCHAR(10) NOT NULL,
-  `mother_name` VARCHAR(100) NOT NULL,
-  `companion` VARCHAR(100) NOT NULL,
-  `address` VARCHAR(100) NOT NULL,
-  `naturalness` VARCHAR(50) NOT NULL,
-  CONSTRAINT `patient_pk` PRIMARY KEY (`cpf`)
+CREATE TABLE patient (
+  cpf VARCHAR(14) NOT NULL,
+  full_name VARCHAR(100) NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  date_of_birth VARCHAR(10) NOT NULL,
+  mother_name VARCHAR(100) NOT NULL,
+  companion VARCHAR(100) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  naturalness VARCHAR(50) NOT NULL,
+  CONSTRAINT patient_pk PRIMARY KEY (cpf)
 );
 
-CREATE TABLE `symptom` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `cpf_patient_fk` VARCHAR(14) NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-   CONSTRAINT `symptom_pk` PRIMARY KEY (`id`),
-   CONSTRAINT `symptom_fk` FOREIGN KEY (`cpf_patient_fk`)
-   REFERENCES `patient` (`cpf`)
+CREATE TABLE symptom (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  cpf_patient_fk VARCHAR(14) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  CONSTRAINT symptom_pk PRIMARY KEY (id),
+  CONSTRAINT symptom_fk FOREIGN KEY (cpf_patient_fk)
+  REFERENCES patient (cpf)
 );
 
 
-CREATE TABLE `medical_records` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `cpf_patient_fk` VARCHAR(14) NOT NULL,
-  `result` FLOAT NOT NULL,
-  `gravity` VARCHAR(50) NOT NULL,
-  `start_date` VARCHAR(10) NOT NULL,
-  CONSTRAINT `medical_records_pk` PRIMARY KEY (`id`),
-  CONSTRAINT `medical_records_fk` FOREIGN KEY (`cpf_patient_fk`)
-  REFERENCES `patient` (`cpf`)
+CREATE TABLE medical_records (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  cpf_patient_fk VARCHAR(14) NOT NULL,
+  result FLOAT NOT NULL,
+  gravity VARCHAR(50) NOT NULL,
+  start_date VARCHAR(10) NOT NULL,
+  CONSTRAINT medical_records_pk PRIMARY KEY (id),
+  CONSTRAINT medical_records_fk FOREIGN KEY (cpf_patient_fk)
+  REFERENCES patient (cpf)
 );
 
-CREATE TABLE `doctor` ( 
-  `id` INT(11) NOT NULL AUTO_INCREMENT, 
-  `name` VARCHAR(100) NOT NULL,
-  `genre` VARCHAR(50) NOT NULL,
-  `specialty` VARCHAR(100) NOT NULL,
-  `active` BOOLEAN DEFAULT 1, 
-  CONSTRAINT `user_pk` PRIMARY KEY (`id`) 
+CREATE TABLE doctor ( 
+  id INT(11) NOT NULL AUTO_INCREMENT, 
+  name VARCHAR(100) NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  specialty VARCHAR(100) NOT NULL,
+  active BOOLEAN DEFAULT 1, 
+  CONSTRAINT user_pk PRIMARY KEY (id) 
 );
 
-CREATE TABLE `medical_appointment` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `cpf_patient_fk` VARCHAR(14) NOT NULL,
-  `id_doctor_fk` INT(11) NOT NULL,
-  `time` VARCHAR(20) NOT NULL,
-  `date` VARCHAR(10) NOT NULL,
-  `arrival_time` INT(11),
-  `realized` BOOLEAN DEFAULT 0,
-  CONSTRAINT `medical_appointment_pk` PRIMARY KEY (`id`),
-  CONSTRAINT `medical_appointment_fk1` FOREIGN KEY (`cpf_patient_fk`)
-  REFERENCES `patient` (`cpf`), 
-  CONSTRAINT `medical_appointment_fk2` FOREIGN KEY (`id_doctor_fk`)
-  REFERENCES `doctor` (`id`) 
+CREATE TABLE medical_appointment (
+  id INT(11) NOT NULL AUTO_INCREMENT,
+  cpf_patient_fk VARCHAR(14) NOT NULL,
+  id_doctor_fk INT(11) NOT NULL,
+  time VARCHAR(20) NOT NULL,
+  date VARCHAR(10) NOT NULL,
+  arrival_time INT(11),
+  realized BOOLEAN DEFAULT 0,
+  CONSTRAINT medical_appointment_pk PRIMARY KEY (id),
+  CONSTRAINT medical_appointment_fk1 FOREIGN KEY (cpf_patient_fk)
+  REFERENCES patient (cpf), 
+  CONSTRAINT medical_appointment_fk2 FOREIGN KEY (id_doctor_fk)
+  REFERENCES doctor (id) 
 );
 
-CREATE TABLE `user` ( 
-  `id` INT(11) NOT NULL AUTO_INCREMENT, 
-  `name` VARCHAR(100) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  `password` VARCHAR(100) NOT NULL, 
-  `responsibility` VARCHAR(50) NOT NULL, 
-  CONSTRAINT `user_pk` PRIMARY KEY (`id`) 
+CREATE TABLE user ( 
+  id INT(11) NOT NULL AUTO_INCREMENT, 
+  name VARCHAR(100) NOT NULL,
+  username VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL, 
+  responsibility VARCHAR(50) NOT NULL, 
+  CONSTRAINT user_pk PRIMARY KEY (id) 
 );
+
+INSERT INTO user (name, username, password, responsibility) 
+VALUES ("Andministrador", "admin", "123", "Administrador");
