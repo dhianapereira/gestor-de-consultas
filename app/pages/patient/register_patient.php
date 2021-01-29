cedcd5f8efd7cc48edc494efa3c57799b6facbec
Dhiana <dhianaslv@outlook.com>
2021-01-29 19:48:00 -0300

    update in the register patient

--- app/pages/patient/register_patient.php
+++ app/pages/patient/register_patient.php
@@ -1,97 +1,122 @@
 <html>
-	<head>
-		<title>Unidade de Saúde | Cadastro</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="#"> 
-        <link
-            rel="stylesheet"
-            type="text/css"
-            href="../../../public/styles/css/main.css"
-        />
-        <link
-            rel="stylesheet"
-            type="text/css"
-            href="../../../public/styles/css/sidebar.css"
-        />
-        <link
-            rel="stylesheet"
-            type="text/css"
-            href="../../../public/styles/css/animations.css"
-        />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap"
-            rel="stylesheet"
-        />
-	</head>
-    <body>
-        <div class="page-pattern">
-            <aside class="animate-right sidebar">
-                <footer>
-                    <a class="sidebar-buttons" href="./register_page.html" title="Cadastrar um novo paciente">
-                        <img src="../../../public/styles/img/plus.svg" alt="Cadastrar um novo paciente" />
-                    </a>
-                    <a class="sidebar-buttons" href="./list_page.php" title="Lista de pacientes">
-                        <img src="../../../public/styles/img/list.svg" alt="Lista de pacientes" />
-                    </a>
-                    <a class="sidebar-buttons" href="#" title="Atualizar Paciente">
-                        <img src="../../../public/styles/img/update-patient.svg" alt="Atualizar Paciente" />
-                    </a>
-                    <a class="sidebar-buttons" href="../home_page.html" title="Home">
-                        <img src="../../../public/styles/img/home.svg" alt="Home" />
-                    </a>
-                </footer>
-            </aside>
-            <main class="animate-appear with-sidebar">
+
+<head>
+    <title>Unidade de Saúde | Cadastro</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="../../../public/styles/img/doctors-list.svg" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="../../../public/styles/css/main.css" />
+    <link rel="stylesheet" type="text/css" href="../../../public/styles/css/home.css" />
+    <link rel="stylesheet" type="text/css" href="../../../public/styles/css/card.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;800&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <header>
+        <h3 class="logo">Unidade de Saúde</h3>
+    </header>
+    <main class="container">
+        <section class="quick-access">
+            <a href="./register_page.html" class="home-button">
+                <h3>
+                    <p>Cadastrar Paciente</p>
+                    <img src="../../../public/styles/img/plus.svg" alt="Imagem de adicionar" />
+                </h3>
+            </a>
+            <a href="./list_page.php" class="home-button">
+                <h3>
+                    <p>Listar Pacientes</p>
+                    <img src="../../../public/styles/img/list.svg" alt="Imagem de lista de pacientes" />
+                </h3>
+            </a>
+            <a href="#" class="home-button">
+                <h3>
+                    <p>Procurar Paciente</p>
+                    <img src="../../../public/styles/img/update-patient.svg" alt="Imagem de paciente" />
+                </h3>
+            </a>
+            <a href="../home_page.php" class="home-button">
+                <h3>
+                    <p>Home</p>
+                    <img src="../../../public/styles/img/home.svg" alt="Imagem de Home" />
+                </h3>
+            </a>
+        </section>
+        <section class="box">
+            <?php
+            include_once('../../utils/autoload.php');
+
+            use app\controllers\PatientController;
+
+            $patient_controller = new PatientController();
+
+            $cpf = addslashes($_POST["cpf"]);
+            $full_name = addslashes($_POST["full_name"]);
+            $genre = $_POST["genre"];
+            $date_of_birth = addslashes($_POST["date_of_birth"]);
+            $mother_name = addslashes($_POST["mother_name"]);
+            $companion = addslashes($_POST["companion"]);
+            $address = addslashes($_POST["address"]);
+            $naturalness = $_POST["naturalness"];
+
+            if (
+                !isset($cpf) || !isset($full_name)
+                || !isset($genre) || !isset($date_of_birth)
+                || !isset($mother_name) || !isset($companion)
+                || !isset($address) || !isset($naturalness)
+            ) {
+            ?>
+                <div class="card">
+                    <h3>
+                        <span>Não foi possível realizar esta operação</span>
+                        <img src="../../../public/styles/img/error.svg" alt="Imagem de mensagem de erro">
+                    </h3>
+                    <p>Você precisa preencher todos os campos para realizar esta operação!</p>
+                </div>
                 <?php
-                    include_once('../../utils/autoload.php');
+            } else {
 
-                    use app\controllers\PatientController;
+                $result = $patient_controller->register(
+                    $cpf,
+                    $full_name,
+                    $genre,
+                    $date_of_birth,
+                    $mother_name,
+                    $companion,
+                    $address,
+                    $naturalness
+                );
 
-                    $patient_controller = new PatientController();
-
-                    $cpf = addslashes($_POST["cpf"]);
-                    $full_name = addslashes($_POST["full_name"]);
-                    $genre = $_POST["genre"];
-                    $date_of_birth = addslashes($_POST["date_of_birth"]);
-                    $mother_name = addslashes($_POST["mother_name"]);
-                    $companion = addslashes($_POST["companion"]);
-                    $address = addslashes($_POST["address"]);
-                    $naturalness = $_POST["naturalness"];
-
-                    if(!isset($cpf) || !isset($full_name)
-                    || !isset($genre) || !isset($date_of_birth) 
-                    || !isset($mother_name) || !isset($companion)
-                    || !isset($address) || !isset($naturalness)){
+                if (!is_object($result)) {
                 ?>
-                        <div class='info'>
-                        <p>Você precisa preencher todos os campos para realizar esta operação!</p>
-                        </div> 
+                    <div class="card">
+                        <h3>
+                            <span>Mensagem de erro</span>
+                            <img src="../../../public/styles/img/error.svg" alt="Imagem de mensagem de erro">
+                        </h3>
+
+                        <p><?php echo "$result" ?></p>
+                    </div>
                 <?php
-                    }
-                    else{
+                } else {
+                ?>
+                    <div class="card">
+                        <h3>
+                            <span>Operação realizada</span>
+                            <img src="../../../public/styles/img/success.svg" alt="Imagem de mensagem de sucesso">
+                        </h3>
+                        <p>O cadastro do paciente foi realizado com sucesso!</p>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </section>
+    </main>
 
-                        $result = $patient_controller->register($cpf, $full_name, 
-                        $genre, $date_of_birth, $mother_name, $companion, $address,
-                        $naturalness);
+    <footer>
+        <p>2021 - Unidade de Saúde</p>
+    </footer>
+</body>
 
-                        if(!is_object($result)){
-                ?>
-                            <div class='info'>
-                                <p><?php echo "$result" ?></p>
-                            </div>    
-                <?php 
-                        } 
-                        else{
-                ?>
-                            <div class='info'>
-                            <p>O cadastro do paciente foi realizado com sucesso!</p>
-                            </div>    
-                <?php 
-                        }
-                    }
-                ?>
-            </main>
-        </div>
-    </body>
 </html>
