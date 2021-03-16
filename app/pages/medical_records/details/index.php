f04a53101711d8d64401ff18d6417ef25631992e
Dhiana <dhianaslv@outlook.com>
2021-03-16 14:17:27 -0300

    Download do prontuario medico

--- app/pages/medical_records/details/index.php
+++ app/pages/medical_records/details/index.php
@@ -61,51 +61,62 @@ require_once "app/components/Base.php";
       <section class="box">
         <div class="form">
           <h2>Dados Pessoais</h2>
-          <form>
+          <form method="POST" action="?class=MedicalRecords&action=download">
             <div class="input-block">
               <label for="full_name">Nome</label>
               <input id="full_name" value="<?php echo ($medical_records->getPatientCpf()[1]) ?>" disabled />
+              <input type="hidden" name="full_name" value="<?php echo ($medical_records->getPatientCpf()[1]) ?>" />
             </div>
             <div class="input-block">
               <label for="cpf">CPF </label>
               <input id="cpf" value="<?php echo ($medical_records->getPatientCpf()[0]) ?>" disabled />
+              <input type="hidden" name="cpf" value="<?php echo ($medical_records->getPatientCpf()[0]) ?>" />
             </div>
             <div class="input-block">
               <label for="date_of_birth">Data de nascimento </label>
               <input id="date_of_birth" value="<?php echo ($medical_records->getPatientCpf()[2]) ?>" disabled />
+              <input type="hidden" name="date_of_birth" value="<?php echo ($medical_records->getPatientCpf()[2]) ?>" />
             </div>
             <div class="input-block">
               <label for="genre">Gênero: </label>
               <input id="genre" value="<?php echo ($medical_records->getPatientCpf()[3]) ?>" disabled />
+              <input type="hidden" name="genre" value="<?php echo ($medical_records->getPatientCpf()[3]) ?>" />
             </div>
             <div class="input-block">
               <label for="naturalness">Naturalidade: </label>
               <input id="naturalness" value="<?php echo ($medical_records->getPatientCpf()[4]) ?>" disabled />
+              <input type="hidden" name="naturalness" value="<?php echo ($medical_records->getPatientCpf()[4]) ?>" />
             </div>
             <div class="input-block">
               <label for="mother_name">Nome da Mãe</label>
               <input id="mother_name" value="<?php echo ($medical_records->getPatientCpf()[6]) ?>" disabled />
+              <input type="hidden" name="mother_name" value="<?php echo ($medical_records->getPatientCpf()[6]) ?>" />
             </div>
             <div class="input-block">
               <label for="companion">Acompanhante</label>
               <input id="companion" value="<?php echo ($medical_records->getPatientCpf()[7]) ?>" disabled />
+              <input type="hidden" name="companion" value="<?php echo ($medical_records->getPatientCpf()[7]) ?>" />
             </div>
             <div class="input-block">
               <label for="patient_address">Endereço</label>
               <input id="patient_address" value="<?php echo ($medical_records->getPatientCpf()[5]) ?>" disabled />
+              <input type="hidden" name="patient_address" value="<?php echo ($medical_records->getPatientCpf()[5]) ?>" />
             </div>
             <h2>Resultados</h2>
             <div class="input-block">
               <label for="start_date">Data de início dos sintomas</label>
               <input id="start_date" value="<?php echo ($medical_records->getStartDate()) ?>" disabled />
+              <input type="hidden" name="start_date" value="<?php echo ($medical_records->getStartDate()) ?>" />
             </div>
             <div class="input-block">
               <label for="result">Resultado (%) </label>
               <input id="result" value="<?php echo (number_format($medical_records->getResult(), 2)) ?>" disabled />
+              <input type="hidden" name="result" value="<?php echo (number_format($medical_records->getResult(), 2)) ?>" />
             </div>
             <div class="input-block">
               <label for="gravity">Gravidade </label>
               <input id="gravity" value="<?php echo ($medical_records->getGravity()) ?>" disabled />
+              <input type="hidden" name="gravity" value="<?php echo ($medical_records->getGravity()) ?>" />
             </div>
             <h2>Sintomas</h2>
             <?php
@@ -121,11 +132,15 @@ require_once "app/components/Base.php";
               ?>
                 <ul>
                   <li><?php echo ($symptom['name']); ?></li>
+                  <input type="hidden" name="symptoms[]" value="<?php echo ($symptom['name']) ?>" />
                 </ul>
             <?php
               }
             }
             ?>
+
+
+            <button type="submit" class="primary-button">Download do Prontuário</button>
           </form>
         </div>
 
